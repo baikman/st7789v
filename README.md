501cd990bd0d5b2f970c96c0954c95180329b814
baikman <baikman@cedarville.edu>
2025-11-02 21:14:27 -0500

    initial helper functions and function outlines

---
+++ README.md
@@ -0,0 +1,3 @@
+### st7789v - pure C driver for the st7789v
+
+(WIP) SPI driver for the st7789v lcd controller for use with rp2XXX series mcus.
---
+++ st7789v.h
@@ -0,0 +1,24 @@
+/*
+ * Free software, check the license, do what you want with it.
+ */
+
+#ifndef ST7789V_H
+#define ST7789V_H
+
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+
+// 240x320
+#define ST7789V_WIDTH 240
+#define ST7789V_HEIGHT 320
+#define ST7789V_DC_PIN 16
+#define ST7789V_RST_PIN 29
+
+// API
+void st7789v_init(spi_inst_t *spi, uint8_t cs_pin, uint8_t sck_pin, uint8_t mosi_pin);
+void st7789v_clear(void);
+void st7789v_update(void);
+void st7789v_blink(uint16_t loops, uint16_t ms);
+void st7789v_write_text(uint8_t textBuff[], uint8_t row, uint8_t column);
+
+#endif
